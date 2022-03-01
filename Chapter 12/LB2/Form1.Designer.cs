1fbee02dca0ff31f2db3448cca51e94ff53a01d7
Sam-Downs <Sam_Downs@insideranken.org>
2022-03-01 08:13:28 -0600

    Updated Tab Order
    Tab order is done

--- Chapter 12/LB2/Form1.Designer.cs
+++ Chapter 12/LB2/Form1.Designer.cs
@@ -107,7 +107,7 @@
             this.groupBox3.Location = new System.Drawing.Point(105, 473);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(552, 254);
-            this.groupBox3.TabIndex = 29;
+            this.groupBox3.TabIndex = 20;
             this.groupBox3.TabStop = false;
             // 
             // lblErrorThree
@@ -116,7 +116,7 @@
             this.lblErrorThree.Location = new System.Drawing.Point(49, 222);
             this.lblErrorThree.Name = "lblErrorThree";
             this.lblErrorThree.Size = new System.Drawing.Size(51, 16);
-            this.lblErrorThree.TabIndex = 26;
+            this.lblErrorThree.TabIndex = 16;
             this.lblErrorThree.Text = "label23";
             // 
             // txtAddDistributor
@@ -124,56 +124,56 @@
             this.txtAddDistributor.Location = new System.Drawing.Point(426, 144);
             this.txtAddDistributor.Name = "txtAddDistributor";
             this.txtAddDistributor.Size = new System.Drawing.Size(100, 22);
-            this.txtAddDistributor.TabIndex = 25;
+            this.txtAddDistributor.TabIndex = 14;
             // 
             // txtAddUnits
             // 
             this.txtAddUnits.Location = new System.Drawing.Point(426, 110);
             this.txtAddUnits.Name = "txtAddUnits";
             this.txtAddUnits.Size = new System.Drawing.Size(100, 22);
-            this.txtAddUnits.TabIndex = 24;
+            this.txtAddUnits.TabIndex = 12;
             // 
             // txtAddCost
             // 
             this.txtAddCost.Location = new System.Drawing.Point(426, 74);
             this.txtAddCost.Name = "txtAddCost";
             this.txtAddCost.Size = new System.Drawing.Size(100, 22);
-            this.txtAddCost.TabIndex = 23;
+            this.txtAddCost.TabIndex = 10;
             // 
             // txtAddKey
             // 
             this.txtAddKey.Location = new System.Drawing.Point(94, 170);
             this.txtAddKey.Name = "txtAddKey";
             this.txtAddKey.Size = new System.Drawing.Size(100, 22);
-            this.txtAddKey.TabIndex = 22;
+            this.txtAddKey.TabIndex = 8;
             // 
             // txtAddPrice
             // 
             this.txtAddPrice.Location = new System.Drawing.Point(94, 136);
             this.txtAddPrice.Name = "txtAddPrice";
             this.txtAddPrice.Size = new System.Drawing.Size(100, 22);
-            this.txtAddPrice.TabIndex = 21;
+            this.txtAddPrice.TabIndex = 6;
             // 
             // txtAddCode
             // 
             this.txtAddCode.Location = new System.Drawing.Point(94, 102);
             this.txtAddCode.Name = "txtAddCode";
             this.txtAddCode.Size = new System.Drawing.Size(100, 22);
-            this.txtAddCode.TabIndex = 20;
+            this.txtAddCode.TabIndex = 4;
             // 
             // txtAddName
             // 
             this.txtAddName.Location = new System.Drawing.Point(94, 68);
             this.txtAddName.Name = "txtAddName";
             this.txtAddName.Size = new System.Drawing.Size(100, 22);
-            this.txtAddName.TabIndex = 15;
+            this.txtAddName.TabIndex = 2;
             // 
             // btnAddItem
             // 
             this.btnAddItem.Location = new System.Drawing.Point(330, 186);
             this.btnAddItem.Name = "btnAddItem";
             this.btnAddItem.Size = new System.Drawing.Size(75, 23);
-            this.btnAddItem.TabIndex = 19;
+            this.btnAddItem.TabIndex = 15;
             this.btnAddItem.Text = "Add Item";
             this.btnAddItem.UseVisualStyleBackColor = true;
             // 
@@ -183,7 +183,7 @@
             this.label22.Location = new System.Drawing.Point(327, 150);
             this.label22.Name = "label22";
             this.label22.Size = new System.Drawing.Size(67, 16);
-            this.label22.TabIndex = 7;
+            this.label22.TabIndex = 13;
             this.label22.Text = "Distributor";
             // 
             // label21
@@ -192,7 +192,7 @@
             this.label21.Location = new System.Drawing.Point(327, 116);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(96, 16);
-            this.label21.TabIndex = 6;
+            this.label21.TabIndex = 11;
             this.label21.Text = "Units Per Case";
             // 
             // label20
@@ -201,7 +201,7 @@
             this.label20.Location = new System.Drawing.Point(327, 77);
             this.label20.Name = "label20";
             this.label20.Size = new System.Drawing.Size(93, 16);
-            this.label20.TabIndex = 5;
+            this.label20.TabIndex = 9;
             this.label20.Text = "Cost Per Case";
             // 
             // label19
@@ -210,7 +210,7 @@
             this.label19.Location = new System.Drawing.Point(6, 176);
             this.label19.Name = "label19";
             this.label19.Size = new System.Drawing.Size(78, 16);
-            this.label19.TabIndex = 4;
+            this.label19.TabIndex = 7;
             this.label19.Text = "Access Key";
             // 
             // label18
@@ -219,7 +219,7 @@
             this.label18.Location = new System.Drawing.Point(6, 142);
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(73, 16);
-            this.label18.TabIndex = 3;
+            this.label18.TabIndex = 5;
             this.label18.Text = "Store Price";
             // 
             // label17
@@ -228,7 +228,7 @@
             this.label17.Location = new System.Drawing.Point(6, 108);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(35, 16);
-            this.label17.TabIndex = 2;
+            this.label17.TabIndex = 3;
             this.label17.Text = "UPC";
             // 
             // label16
@@ -261,7 +261,7 @@
             this.groupBox2.Location = new System.Drawing.Point(413, 214);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(244, 233);
-            this.groupBox2.TabIndex = 28;
+            this.groupBox2.TabIndex = 19;
             this.groupBox2.TabStop = false;
             // 
             // label28
@@ -270,7 +270,7 @@
             this.label28.Location = new System.Drawing.Point(88, 18);
             this.label28.Name = "label28";
             this.label28.Size = new System.Drawing.Size(75, 16);
-            this.label28.TabIndex = 37;
+            this.label28.TabIndex = 0;
             this.label28.Text = "Delete Item";
             // 
             // txtDeleteKey
@@ -278,14 +278,14 @@
             this.txtDeleteKey.Location = new System.Drawing.Point(119, 97);
             this.txtDeleteKey.Name = "txtDeleteKey";
             this.txtDeleteKey.Size = new System.Drawing.Size(100, 22);
-            this.txtDeleteKey.TabIndex = 26;
+            this.txtDeleteKey.TabIndex = 4;
             // 
             // txtDeleteCode
             // 
             this.txtDeleteCode.Location = new System.Drawing.Point(119, 45);
             this.txtDeleteCode.Name = "txtDeleteCode";
             this.txtDeleteCode.Size = new System.Drawing.Size(100, 22);
-            this.txtDeleteCode.TabIndex = 25;
+            this.txtDeleteCode.TabIndex = 2;
             // 
             // lblErrorTwo
             // 
@@ -293,14 +293,14 @@
             this.lblErrorTwo.Location = new System.Drawing.Point(62, 202);
             this.lblErrorTwo.Name = "lblErrorTwo";
             this.lblErrorTwo.Size = new System.Drawing.Size(0, 16);
-            this.lblErrorTwo.TabIndex = 19;
+            this.lblErrorTwo.TabIndex = 6;
             // 
             // btnDelete
             // 
             this.btnDelete.Location = new System.Drawing.Point(65, 155);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(111, 23);
-            this.btnDelete.TabIndex = 24;
+            this.btnDelete.TabIndex = 5;
             this.btnDelete.Text = "Delete Item";
             this.btnDelete.UseVisualStyleBackColor = true;
             // 
@@ -310,7 +310,7 @@
             this.label14.Location = new System.Drawing.Point(19, 48);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(72, 16);
-            this.label14.TabIndex = 21;
+            this.label14.TabIndex = 1;
             this.label14.Text = "Verify UPC";
             // 
             // label13
@@ -319,7 +319,7 @@
             this.label13.Location = new System.Drawing.Point(22, 100);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(78, 16);
-            this.label13.TabIndex = 22;
+            this.label13.TabIndex = 3;
             this.label13.Text = "Access Key";
             // 
             // groupBox1
@@ -334,7 +334,7 @@
             this.groupBox1.Location = new System.Drawing.Point(105, 214);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(244, 233);
-            this.groupBox1.TabIndex = 27;
+            this.groupBox1.TabIndex = 18;
             this.groupBox1.TabStop = false;
             // 
             // label27
@@ -343,7 +343,7 @@
             this.label27.Location = new System.Drawing.Point(61, 18);
             this.label27.Name = "label27";
             this.label27.Size = new System.Drawing.Size(123, 16);
-            this.label27.TabIndex = 36;
+            this.label27.TabIndex = 0;
             this.label27.Text = "Change Store Price";
             // 
             // txtUpdateAccessKey
@@ -351,14 +351,14 @@
             this.txtUpdateAccessKey.Location = new System.Drawing.Point(119, 97);
             this.txtUpdateAccessKey.Name = "txtUpdateAccessKey";
             this.txtUpdateAccessKey.Size = new System.Drawing.Size(100, 22);
-            this.txtUpdateAccessKey.TabIndex = 28;
+            this.txtUpdateAccessKey.TabIndex = 4;
             // 
             // txtUpdatePrice
             // 
             this.txtUpdatePrice.Location = new System.Drawing.Point(119, 42);
             this.txtUpdatePrice.Name = "txtUpdatePrice";
             this.txtUpdatePrice.Size = new System.Drawing.Size(100, 22);
-            this.txtUpdatePrice.TabIndex = 27;
+            this.txtUpdatePrice.TabIndex = 2;
             // 
             // lblErrorOne
             // 
@@ -366,14 +366,14 @@
             this.lblErrorOne.Location = new System.Drawing.Point(49, 202);
             this.lblErrorOne.Name = "lblErrorOne";
             this.lblErrorOne.Size = new System.Drawing.Size(0, 16);
-            this.lblErrorOne.TabIndex = 15;
+            this.lblErrorOne.TabIndex = 6;
             // 
             // btnUpdate
             // 
             this.btnUpdate.Location = new System.Drawing.Point(52, 155);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(110, 23);
-            this.btnUpdate.TabIndex = 18;
+            this.btnUpdate.TabIndex = 5;
             this.btnUpdate.Text = "Update Price";
             this.btnUpdate.UseVisualStyleBackColor = true;
             // 
@@ -383,7 +383,7 @@
             this.label10.Location = new System.Drawing.Point(31, 100);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(78, 16);
-            this.label10.TabIndex = 16;
+            this.label10.TabIndex = 3;
             this.label10.Text = "Access Key";
             // 
             // label9
@@ -392,7 +392,7 @@
             this.label9.Location = new System.Drawing.Point(31, 45);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(68, 16);
-            this.label9.TabIndex = 15;
+            this.label9.TabIndex = 1;
             this.label9.Text = "New Price";
             // 
             // label6
@@ -401,7 +401,7 @@
             this.label6.Location = new System.Drawing.Point(410, 92);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(96, 16);
-            this.label6.TabIndex = 26;
+            this.label6.TabIndex = 8;
             this.label6.Text = "Cost Per Case:";
             // 
             // label7
@@ -410,7 +410,7 @@
             this.label7.Location = new System.Drawing.Point(410, 132);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(99, 16);
-            this.label7.TabIndex = 25;
+            this.label7.TabIndex = 12;
             this.label7.Text = "Units Per Case:";
             // 
             // label8
@@ -419,7 +419,7 @@
             this.label8.Location = new System.Drawing.Point(410, 172);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(73, 16);
-            this.label8.TabIndex = 24;
+            this.label8.TabIndex = 16;
             this.label8.Text = "Distributor: ";
             // 
             // label5
@@ -428,7 +428,7 @@
             this.label5.Location = new System.Drawing.Point(102, 172);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(76, 16);
-            this.label5.TabIndex = 23;
+            this.label5.TabIndex = 14;
             this.label5.Text = "Store Price:";
             // 
             // label4
@@ -437,7 +437,7 @@
             this.label4.Location = new System.Drawing.Point(102, 132);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(38, 16);
-            this.label4.TabIndex = 22;
+            this.label4.TabIndex = 10;
             this.label4.Text = "UPC:";
             // 
             // label3
@@ -446,7 +446,7 @@
             this.label3.Location = new System.Drawing.Point(102, 92);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(47, 16);
-            this.label3.TabIndex = 21;
+            this.label3.TabIndex = 6;
             this.label3.Text = "Name:";
             // 
             // btnCodeSearch
@@ -454,7 +454,7 @@
             this.btnCodeSearch.Location = new System.Drawing.Point(532, 40);
             this.btnCodeSearch.Name = "btnCodeSearch";
             this.btnCodeSearch.Size = new System.Drawing.Size(75, 23);
-            this.btnCodeSearch.TabIndex = 20;
+            this.btnCodeSearch.TabIndex = 5;
             this.btnCodeSearch.Text = "Search";
             this.btnCodeSearch.UseVisualStyleBackColor = true;
             // 
@@ -463,7 +463,7 @@
             this.txtSearchByCode.Location = new System.Drawing.Point(413, 40);
             this.txtSearchByCode.Name = "txtSearchByCode";
             this.txtSearchByCode.Size = new System.Drawing.Size(100, 22);
-            this.txtSearchByCode.TabIndex = 19;
+            this.txtSearchByCode.TabIndex = 4;
             // 
             // label2
             // 
@@ -471,7 +471,7 @@
             this.label2.Location = new System.Drawing.Point(410, 12);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(99, 16);
-            this.label2.TabIndex = 18;
+            this.label2.TabIndex = 3;
             this.label2.Text = "Search by UPC";
             // 
             // btnNameSearch
@@ -479,7 +479,7 @@
             this.btnNameSearch.Location = new System.Drawing.Point(224, 39);
             this.btnNameSearch.Name = "btnNameSearch";
             this.btnNameSearch.Size = new System.Drawing.Size(75, 23);
-            this.btnNameSearch.TabIndex = 17;
+            this.btnNameSearch.TabIndex = 2;
             this.btnNameSearch.Text = "Search";
             this.btnNameSearch.UseVisualStyleBackColor = true;
             // 
@@ -488,7 +488,7 @@
             this.txtSearchByName.Location = new System.Drawing.Point(105, 39);
             this.txtSearchByName.Name = "txtSearchByName";
             this.txtSearchByName.Size = new System.Drawing.Size(100, 22);
-            this.txtSearchByName.TabIndex = 16;
+            this.txtSearchByName.TabIndex = 1;
             // 
             // label1
             // 
@@ -496,7 +496,7 @@
             this.label1.Location = new System.Drawing.Point(102, 11);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(108, 16);
-            this.label1.TabIndex = 15;
+            this.label1.TabIndex = 0;
             this.label1.Text = "Search by Name";
             // 
             // lblName
@@ -505,7 +505,7 @@
             this.lblName.Location = new System.Drawing.Point(155, 92);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(0, 16);
-            this.lblName.TabIndex = 30;
+            this.lblName.TabIndex = 7;
             // 
             // lblCode
             // 
@@ -513,7 +513,7 @@
             this.lblCode.Location = new System.Drawing.Point(146, 132);
             this.lblCode.Name = "lblCode";
             this.lblCode.Size = new System.Drawing.Size(0, 16);
-            this.lblCode.TabIndex = 31;
+            this.lblCode.TabIndex = 11;
             // 
             // lblStorePrice
             // 
@@ -521,7 +521,7 @@
             this.lblStorePrice.Location = new System.Drawing.Point(184, 172);
             this.lblStorePrice.Name = "lblStorePrice";
             this.lblStorePrice.Size = new System.Drawing.Size(0, 16);
-            this.lblStorePrice.TabIndex = 32;
+            this.lblStorePrice.TabIndex = 15;
             // 
             // lblCostPerCase
             // 
@@ -529,7 +529,7 @@
             this.lblCostPerCase.Location = new System.Drawing.Point(512, 92);
             this.lblCostPerCase.Name = "lblCostPerCase";
             this.lblCostPerCase.Size = new System.Drawing.Size(0, 16);
-            this.lblCostPerCase.TabIndex = 33;
+            this.lblCostPerCase.TabIndex = 9;
             // 
             // lblUnitsPerCase
             // 
@@ -537,7 +537,7 @@
             this.lblUnitsPerCase.Location = new System.Drawing.Point(515, 132);
             this.lblUnitsPerCase.Name = "lblUnitsPerCase";
             this.lblUnitsPerCase.Size = new System.Drawing.Size(0, 16);
-            this.lblUnitsPerCase.TabIndex = 34;
+            this.lblUnitsPerCase.TabIndex = 13;
             // 
             // lblDistributor
             // 
@@ -545,7 +545,7 @@
             this.lblDistributor.Location = new System.Drawing.Point(489, 172);
             this.lblDistributor.Name = "lblDistributor";
             this.lblDistributor.Size = new System.Drawing.Size(0, 16);
-            this.lblDistributor.TabIndex = 35;
+            this.lblDistributor.TabIndex = 17;
             // 
             // Form1
             // 
