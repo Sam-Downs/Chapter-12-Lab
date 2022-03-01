ee8c3611871957f7ed73e5d049a070d47ca7aacf
Amel Topalovic <Amel_Topalovic@insideranken.org>
2022-03-01 09:53:13 -0600

    created class, and wired buttons

--- Chapter 12/LB2/Form1.cs
+++ Chapter 12/LB2/Form1.cs
@@ -16,5 +16,42 @@ namespace LB2
         {
             InitializeComponent();
         }
+
+        private void btnNameSearch_Click(object sender, EventArgs e)
+        {
+            string nameSearch = txtSearchByName.Text;
+        }
+
+        private void btnCodeSearch_Click(object sender, EventArgs e)
+        {
+            string codeSearch = txtSearchByCode.Text;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            decimal newPrice = decimal.Parse(txtUpdatePrice.Text);
+            string accessKey = txtUpdateAccessKey.Text;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string verifyUPC = txtDeleteCode.Text;
+            string accessKey = txtDeleteKey.Text;
+        }
+
+        private void btnAddItem_Click(object sender, EventArgs e)
+        {
+            string nameAdd = txtAddName.Text;
+            string codeAdd = txtAddCode.Text;
+            decimal priceAdd = decimal.Parse(txtAddPrice.Text);
+            string accessKey = txtAddKey.Text;
+            decimal addCostPerCase = decimal.Parse(txtAddCost.Text);
+            int addUnitsPerCase = int.Parse(txtAddUnits.Text);
+            string distributor = txtAddDistributor.Text;
+
+
+
+
+        }
     }
 }
--- Chapter 12/LB2/Item.cs
+++ Chapter 12/LB2/Item.cs
@@ -16,9 +16,47 @@ namespace LB2
         private decimal _costPerCase;
         private string _distributor;
 
+        public Item(string name, string upc, decimal price, string accessKey, int unitsPerCase, decimal costPerCase, string distributor)
+        {
+            _name = name;
+            _upc = upc;
+            _price = price;
+            _accessKey = accessKey;
+            _unitsPerCase = unitsPerCase;
+            _costPerCase = costPerCase;
+            _distributor = distributor;
+        }
 
-
-
+       public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string UPC
+        {
+            get { return _upc; }
+            set { _upc = value; }
+        }
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public int UnitsPerCase
+        {
+            get { return _unitsPerCase; }
+            set { _unitsPerCase = value; }
+        }
+        public decimal CostPerCase
+        {
+            get { return _costPerCase; }
+            set { _costPerCase = value; }
+        }
+        public string Distributor
+        {
+            get { return _distributor; }
+            set { _distributor = value; }
+        }
 
 
 
